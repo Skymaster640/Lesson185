1e1ece2418c37396bb631d93a2cbbfff7f527f95
Skymaster640 <skylar.TheHouseRobinson@gmail.com>
2023-02-04 13:13:46 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Main from "./screens/Main";
+import Home from "./screens/Home"
+
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName = "Home" screenOptions={{headerShown:false}}>
+        <Stack.Screen
+          name="Home"
+          component={Home}/>
+          <Stack.Screen
+          name="Main"
+          component={Main}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
